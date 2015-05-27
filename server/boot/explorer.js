e0809122556f2045c6be16d544768bfba63a84ce
Andrés Amado <acactown@gmail.com>
2015-05-26 23:34:21 -0300

    Add swagger bootenv config files
    Signed-off-by: Andrés Amado <acactown@gmail.com>

--- server/boot/explorer.js
+++ server/boot/explorer.js
@@ -1,27 +1,30 @@
-module.exports = function mountLoopBackExplorer(server) {
-  var explorer;
-  try {
-    explorer = require('loopback-explorer');
-  } catch(err) {
-    // Print the message only when the app was started via `server.listen()`.
-    // Do not print any message when the project is used as a component.
-    server.once('started', function(baseUrl) {
-      console.log(
-        'Run `npm install loopback-explorer` to enable the LoopBack explorer'
-      );
-    });
-    return;
+'use strict';
+
+var environment = require('../utils/environment'),
+    path        = require('path'),
+    log         = require('../utils/logger');
+
+module.exports = function (app) {
+  if (environment.isDev()) {
+    try {
+      var explorer    = require('loopback-explorer'),
+          restApiRoot = app.get('restApiRoot');
+
+      var explorerApp = explorer(app, {
+        uiDirs: path.resolve(__dirname, '../../client'),
+        basePath: restApiRoot,
+        disableStackTrace: true
+      });
+
+      app.use('/explorer', explorerApp);
+      app.once('started', function () {
+        var baseUrl      = app.get('url').replace(/\/$/, ''),
+            explorerPath = explorerApp.mountpath || explorerApp.route;
+
+        log.info('Browse your REST API at %s%s', baseUrl, explorerPath);
+      });
+    } catch (error) {
+      log.error('Run `npm install loopback-explorer` to enable the LoopBack explorer');
+    }
   }
-
-  var restApiRoot = server.get('restApiRoot');
-
-  var explorerApp = explorer(server, { basePath: restApiRoot });
-  server.use('/explorer', explorerApp);
-  server.once('started', function() {
-    var baseUrl = server.get('url').replace(/\/$/, '');
-    // express 4.x (loopback 2.x) uses `mountpath`
-    // express 3.x (loopback 1.x) uses `route`
-    var explorerPath = explorerApp.mountpath || explorerApp.route;
-    console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-  });
 };
