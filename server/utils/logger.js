93a51c9efff31cc11d77719b31011da5cd687262
Andrés Amado <acactown@gmail.com>
2015-05-26 23:10:30 -0300

    Add simple logger
    Signed-off-by: Andrés Amado <acactown@gmail.com>

---
+++ server/utils/logger.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var environment = require('./environment'),
+    winston     = require('winston');
+
+var config = {
+  colors: {
+    trace: 'magenta',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    debug: 'blue',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    error: 'red'
+  },
+  transports: [],
+  exitOnError: false
+};
+
+if (environment.isProd()) {
+  config.transports.push(
+    new winston.transports.Console({
+      level: 'warn'
+    })
+  );
+} else {
+  config.transports.push(
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      timestamp: true,
+      stringify: false,
+      json: false,
+      prettyPrint: true,
+      colorize: true,
+      silent: false,
+      humanReadableUnhandledException: true
+    })
+  );
+}
+
+var log = new winston.Logger(config);
+
+winston.emitErrs = true;
+
+log.debug('Running on [%s] environment.', environment.getName());
+
+module.exports = log;
